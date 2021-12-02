c186784af9020626251e9d16126efdf5828070fd
saeed <sa.ghofraniivari@gmail.com>
2021-12-02 10:02:32 +0330

    local express

--- 33 09.11/express/main.js
+++ 33 09.11/express/main.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(true);
+});
+
+
+
+app.listen(5000);
+console.log('server listening on port 5000');
